d762ddc7a46c0c7875d00dc46c09a49b0d98b74d
alrivasp <alvaro.rivas.p@gmail.com>
2020-05-12 19:10:34 -0400

    UP desafio Adicto a redes v2

---
+++ adictos_a_redes2.rb
@@ -0,0 +1,14 @@
+def scan_addicts2(array)
+    resultado = []
+    array.count.times do |i|
+        if array[i] < 90
+            resultado.push("bien")
+        elsif array[i] > 90 && array[i] < 180
+            resultado.push("mejorable")
+        else 
+            resultado.push("mal")
+        end
+    end
+    resultado
+end
+print scan_addicts2([120, 50, 600, 30, 90, 10, 200, 0, 500])
